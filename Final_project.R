007a5fb9de42d039692cd0fb44f2a2974cae3d98
Asbjørn Frederik Kloppenborg <au557539@uni.au.dk>
2024-08-22 13:49:39 +0200

    First upload

---
+++ Final_project.R
@@ -0,0 +1,14 @@
+#Read all data from the NHL df
+data <- tidytuesdayR::tt_load("2024-01-09")
+
+#Extract the roster information
+rosters <- data$nhl_rosters
+
+#Extract information regarding players birthdate
+nhl_player_births <- data$nhl_player_births
+
+#Extract information regarding all births in Canada
+canada_births <- data$canada_births_1991_2022
+
+#Extract information regarding name on NHL teams
+nhl_teams <- data$nhl_teams
